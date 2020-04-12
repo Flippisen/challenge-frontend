216283d430bb868bae28255ea45144d9146c1784
Philip Nilsson <philipn@apadmi.com>
2020-04-12 21:12:21 +0100

    Actually added login button to login component

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,24 +4,28 @@
             <label>
                 {{ $t('Login.Username') }}
             </label>
-            <md-input v-model="username"></md-input>
+            <md-input v-model="username" required></md-input>
         </md-field>
 
         <md-field>
             <label>
                 {{ $t('Login.Password') }}
             </label>
-            <md-input v-model="password"></md-input>
+            <md-input v-model="password" required type='password'></md-input>
         </md-field>
+
+        <md-button type='submit' class='md-primary md-raised'>{{ $t('Login.Login') }}</md-button>
     </div>
 </template>
 
-<script>
-export default {
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
   name: 'LoginForm',
   data: () => ({
     username: '',
     password: '',
   }),
-};
+});
 </script>
