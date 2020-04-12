9cf454a718e8447990bfe45a4b704c287ee1a7e8
Philip Nilsson <philipn@apadmi.com>
2020-04-12 21:01:41 +0100

    Added login form

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import Vue from 'vue';
+// eslint-disable-next-line
+// @ts-ignore
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import i18n from './i18n'
+import i18n from './i18n';
+
 
 Vue.config.productionTip = false;
+Vue.use(VueMaterial);
 
 new Vue({
   router,
   store,
   i18n,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class='login-container'>
+      <div class='login-form'>
+        <LoginForm />
+      </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .login-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    max-width: 100%;
+    max-height: 100%;
+    background-color: #ff0000;
+  }
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import LoginForm from '@/components/LoginForm.vue';
+
+export default Vue.extend({
+  name: 'Login',
+  components: {
+    LoginForm,
+  },
+});
+</script>
