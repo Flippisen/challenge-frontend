1ae4a701bcc6b830edb9320e9c17e177371283cf
Philip Nilsson <philipn@apadmi.com>
2020-04-12 20:30:57 +0100

    Added i18n and added login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <md-field>
+            <label>
+                {{ $t('Login.Username') }}
+            </label>
+            <md-input v-model="username"></md-input>
+        </md-field>
+
+        <md-field>
+            <label>
+                {{ $t('Login.Password') }}
+            </label>
+            <md-input v-model="password"></md-input>
+        </md-field>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    username: '',
+    password: '',
+  }),
+};
+</script>
