f2ac8f7572e5f24c497437fd900d42276357ddd5
Philip Nilsson <philip@nilsson.gi>
2020-04-13 12:39:36 +0100

    Added routing for dashboard component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,24 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Login from '@/views/auth/Login.vue';
+import Dashboard from '@/views/Dashboard.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
+    name: 'Dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '*',
+    redirect: '/',
   },
 ];
 
